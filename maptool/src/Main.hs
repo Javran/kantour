7f4b52b51eb05bf215deff41241300f57fd6c967
Javran Cheng <Javran.c@gmail.com>
2017-02-09 22:14:59 -0500

    regulations

--- maptool/src/Main.hs
+++ maptool/src/Main.hs
@@ -25,8 +25,10 @@ and perhaps more.
 
 -}
 
-data Point = Point Int Int
-  deriving Show
+data Point = Point
+  { ptX :: Int
+  , ptY :: Int
+  } deriving Show
 
 data Line = Line
   { lName :: String
@@ -34,68 +36,64 @@ data Line = Line
   , lEnd :: Point
   } deriving Show
 
-getX, getY :: Point -> Int
-
-getX (Point x _) = x
-getY (Point _ y) = y
-
 getRange :: [Line] -> ((Int,Int),(Int,Int))
 getRange xs = ((minimum xCoords, maximum xCoords),(minimum yCoords,maximum yCoords))
   where
-    xCoords = concatMap (\(Line _ p1 p2) -> maybeToList (getX <$> p1) ++ [getX p2]) xs :: [Int]
-    yCoords = concatMap (\(Line _ p1 p2) -> maybeToList (getY <$> p1) ++ [getY p2]) xs :: [Int]
+    xCoords = concatMap (\(Line _ p1 p2) -> maybeToList (ptX <$> p1) ++ [ptX p2]) xs :: [Int]
+    yCoords = concatMap (\(Line _ p1 p2) -> maybeToList (ptY <$> p1) ++ [ptY p2]) xs :: [Int]
+
+getRoute :: IOSArrow XmlTree Line
+getRoute = proc doc -> do
+    rawCId <- deep (hasName "item" >>>
+                    hasAttrValue "name" (== "map") >>>
+                    getAttrValue "characterId") -< doc
+    lineRef <- deep (hasName "item" >>>
+                     hasAttrValue "spriteId" (== rawCId)) />
+               hasName "subTags" />
+               hasName "item" -<< doc
+    line <- hasAttrValue "name" ("line" `isPrefixOf`) -<< lineRef
+    lineName <- getAttrValue "name" -< line
+    mat <- this /> hasName "matrix" -< line
+    ptEnd <- getIntPair "translateX" "translateY" Point -< mat
+    spriteId <- getAttrValue "characterId" -< lineRef
+    sprite <- deep (hasName "item" >>>
+                    hasAttrValue "spriteId" (== spriteId)) -<< doc
+    shapeRef <- this /> hasName "subTags"
+                     /> hasAttrValue "characterId" (pure True) -< sprite
+    shapeId <- getAttrValue "characterId" -< shapeRef
+    matSp <- this /> hasName "matrix" -< shapeRef
+    (shX,shY) <- getIntPair "translateX" "translateY" (,) -< matSp
+    shape <- deep (hasName "item" >>>
+                   hasAttrValue "shapeId" (== shapeId) />
+                   hasName "shapeBounds")
+             -<< doc
+    (xMax,xMin) <- getIntPair "Xmax" "Xmin" (,) -< shape
+    (yMax,yMin) <- getIntPair "Ymax" "Ymin" (,) -< shape
+    let dx = (shX + ((xMax + xMin) `div` 2)) * 2
+    let dy = (shY + ((yMax + yMin) `div` 2)) * 2
+        mPtStart = if dx == 0 && dy == 0
+                     then Nothing
+                     else Just (Point (ptX ptEnd+dx) (ptY ptEnd+dy))
+    this -< Line lineName mPtStart ptEnd
+  where
+    asInt = arr (read :: String -> Int)
+    getIntPair :: String -> String -> (Int -> Int -> a) -> _ _ a
+    getIntPair fstName sndName resultF =
+        (   (getAttrValue fstName >>> asInt)
+        &&& (getAttrValue sndName >>> asInt)
+        ) >>> arr (uncurry resultF)
 
 main :: IO ()
 main = do
     [srcFP] <- getArgs
-    results <- runX (readDocument [] srcFP >>> proc doc -> do
-                         rawCId <- deep (hasName "item" >>>
-                                         hasAttrValue "name" (== "map") >>>
-                                         getAttrValue "characterId") -< doc
-                         lineRef <- deep (hasName "item" >>>
-                                          hasAttrValue "spriteId" (== rawCId)) />
-                                    hasName "subTags" />
-                                    hasName "item" -<< doc
-                         line <- hasAttrValue "name" ("line" `isPrefixOf`) -<< lineRef
-                         lineName <- getAttrValue "name" -< line
-                         mat <- returnA /> hasName "matrix" -< line
-                         x <- getAttrValue "translateX" >>> asInt -< mat
-                         y <- getAttrValue "translateY" >>> asInt  -< mat
-                         let ptEnd = Point x y
-                         spriteId <- getAttrValue "characterId" -< lineRef
-                         sprite <- deep (hasName "item" >>>
-                                         hasAttrValue "spriteId" (== spriteId)) -<< doc
-                         shapeRef <- returnA /> hasName "subTags"
-                                     /> hasAttrValue "characterId" (pure True) -< sprite
-                         shapeId <- getAttrValue "characterId" -< shapeRef
-                         matSp <- returnA /> hasName "matrix" -< shapeRef
-                         shX <- getAttrValue "translateX" >>> asInt -< matSp
-                         shY <- getAttrValue "translateY" >>> asInt -< matSp
-
-                         shape <- deep (hasName "item" >>>
-                                        hasAttrValue "shapeId" (== shapeId) />
-                                        hasName "shapeBounds")
-                                  -<< doc
-                         xMax <- getAttrValue "Xmax" >>> asInt -< shape
-                         xMin <- getAttrValue "Xmin" >>> asInt -< shape
-
-                         yMax <- getAttrValue "Ymax" >>> asInt -< shape
-                         yMin <- getAttrValue "Ymin" >>> asInt -< shape
-                         let dx = (shX + ((xMax + xMin) `div` 2)) * 2
-                         let dy = (shY + ((yMax + yMin) `div` 2)) * 2
-                             mPtStart = if dx == 0 && dy == 0
-                                          then Nothing
-                                          else Just (Point (x+dx) (y+dy))
-                         returnA -< Line lineName mPtStart ptEnd
-                    )
+    results <- runX (readDocument [] srcFP >>> getRoute)
     mapM_ print results
     print (getRange results)
 
     {-
     -- for gnuplot
     forM_ results $ \(Line _ (Just ptStart) ptEnd) -> do
-        putStrLn $ show (getX ptStart) ++ " " ++ show (getY ptStart)
-        putStrLn $ show (getX ptEnd) ++ " " ++ show (getY ptEnd)
+        putStrLn $ show (ptX ptStart) ++ " " ++ show (ptY ptStart)
+        putStrLn $ show (ptX ptEnd) ++ " " ++ show (ptY ptEnd)
         putStrLn "" -}
-  where
-    asInt = arr (read :: String -> Int)
+
