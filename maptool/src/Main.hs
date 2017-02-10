70227b63918a38236fa3fa1fc34f8a15416bd466
Javran Cheng <Javran.c@gmail.com>
2017-02-10 00:19:12 -0500

    few more comments, extract start point

--- maptool/src/Main.hs
+++ maptool/src/Main.hs
@@ -23,6 +23,12 @@ in Haskell
 
 and perhaps more.
 
+related links:
+
+an explanation can be found at:
+
+http://blog.dazzyd.org/blog/how-to-draw-a-kancolle-map/
+
 -}
 
 data Point = Point
@@ -42,26 +48,34 @@ getRange xs = ((minimum xCoords, maximum xCoords),(minimum yCoords,maximum yCoor
     xCoords = concatMap (\(Line _ p1 p2) -> maybeToList (ptX <$> p1) ++ [ptX p2]) xs :: [Int]
     yCoords = concatMap (\(Line _ p1 p2) -> maybeToList (ptY <$> p1) ++ [ptY p2]) xs :: [Int]
 
-getRoute :: IOSArrow XmlTree Line
+getRoute :: IOSArrow XmlTree _
 getRoute = proc doc -> do
     rawCId <- deep (hasName "item" >>>
                     hasAttrValue "name" (== "map") >>>
                     getAttrValue "characterId") -< doc
-    lineRef <- deep (hasName "item" >>>
+    lineRefs <- deep (hasName "item" >>>
                      hasAttrValue "spriteId" (== rawCId)) />
-               hasName "subTags" />
-               hasName "item" -<< doc
-    line <- hasAttrValue "name" ("line" `isPrefixOf`) -<< lineRef
+               hasName "subTags" /> hasName "item" -<< doc
+    -- each of these item corresponds to a line (route)
+    line <- hasAttrValue "name" ("line" `isPrefixOf`) -<< lineRefs
+
     lineName <- getAttrValue "name" -< line
     mat <- this /> hasName "matrix" -< line
+    -- end point coordinate (or the origin of the "item" resource)
     ptEnd <- getIntPair "translateX" "translateY" Point -< mat
-    spriteId <- getAttrValue "characterId" -< lineRef
+
+    -- gettling line sprite
+    spriteId <- getAttrValue "characterId" -< lineRefs
     sprite <- deep (hasName "item" >>>
                     hasAttrValue "spriteId" (== spriteId)) -<< doc
+    -- shape should be a child of sprite (the line)
     shapeRef <- this /> hasName "subTags"
                      /> hasAttrValue "characterId" (pure True) -< sprite
+
     shapeId <- getAttrValue "characterId" -< shapeRef
     matSp <- this /> hasName "matrix" -< shapeRef
+    -- (TODO) actually it's suppose to be the case that "(shX,shY) + endPoint" is the real node point..
+    -- but let's not worry about that right now.
     (shX,shY) <- getIntPair "translateX" "translateY" (,) -< matSp
     shape <- deep (hasName "item" >>>
                    hasAttrValue "shapeId" (== shapeId) />
@@ -74,7 +88,9 @@ getRoute = proc doc -> do
         mPtStart = if dx == 0 && dy == 0
                      then Nothing
                      else Just (Point (ptX ptEnd+dx) (ptY ptEnd+dy))
-    this -< Line lineName mPtStart ptEnd
+    startMat <- deep (hasAttrValue "name" (== "line0")) /> hasName "matrix" -< doc
+    ptMapStart <- getIntPair "translateX" "translateY" Point -< startMat
+    this -< (Line lineName mPtStart ptEnd, ptMapStart)
   where
     asInt = arr (read :: String -> Int)
     getIntPair :: String -> String -> (Int -> Int -> a) -> _ _ a
@@ -88,7 +104,9 @@ main = do
     [srcFP] <- getArgs
     results <- runX (readDocument [] srcFP >>> getRoute)
     mapM_ print results
-    print (getRange results)
+    -- the coordinates look like large numbers because SWF uses twip as basic unit
+    -- (most of the time) divide them by 20 to get pixels
+    -- print (getRange results)
 
     {-
     -- for gnuplot
