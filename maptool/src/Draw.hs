13ce789d87a38be846eb2f27c06afb2450e98d75
Javran Cheng <Javran.c@gmail.com>
2017-02-13 23:27:03 -0500

    try using coerce rather than explicit coversion

--- maptool/src/Draw.hs
+++ maptool/src/Draw.hs
@@ -9,6 +9,7 @@ import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
 import Data.Maybe
 import qualified Data.Map.Strict as M
+import Data.Coerce
 
 import Types
 
@@ -28,14 +29,14 @@ drawKCMap xs pm = (mconcat [greenPoints, redPoints]
     greenPoints = flip atPoints (map (\l ->
                                       cirGreen (fromMaybe "?" (M.lookup (_lEnd l) pm))
                                                 # named (_lName l ++ "g")) xs)
-                  $ reflectY ( map p2 (convertPt <$> map _lEnd xs))
+                  $ reflectY (map p2 (convertPt <$> map _lEnd xs))
     redPoints = flip atPoints (map (\l -> cirRed (fromMaybe "?" (M.lookup (fromJust $ _lStart l) pm))
                                # named (_lName l ++ "r")) xs)
-                $ reflectY ( map p2 (convertPt <$> map (fromJust . _lStart) xs))
+                $ reflectY (map p2 (convertPt <$> map (fromJust . _lStart) xs))
     convertPt (V2 x y) = (twipToPixel x, twipToPixel y)
     arrowOpts = with & gaps .~ small & headLength .~ 22
     midPoints :: [Point V2 Double]
-    midPoints = reflectY <$> (((\pt -> (pt^._x) ^& (pt^._y)) . lineMid) <$> xs)
+    midPoints = reflectY <$> ((coerce . lineMid) <$> xs)
     arrLbls = map (\l -> text (simpleLName l) # fc blue # fontSizeL 16) xs
 
 draw :: [MyLine] -> M.Map (V2 Int) String -> IO ()
