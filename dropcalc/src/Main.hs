c25e768fab64fbae88f086dcef1d00cc62291270
Javran Cheng <Javran.c@gmail.com>
2017-02-17 22:57:56 -0500

    impl experiment and provide simple statistics

--- dropcalc/src/Main.hs
+++ dropcalc/src/Main.hs
@@ -2,6 +2,9 @@ module Main where
 
 import System.Environment
 import Control.Monad
+import Control.Monad.Primitive
+import System.Random.MWC
+import Text.Printf
 
 -- either a number between 0 and 1
 -- or {x}% where x can be between 0 and 100
@@ -22,12 +25,35 @@ parseCount xs
     , v > 0 = Just v
     | otherwise = Nothing
 
+experiment :: PrimMonad m => Gen (PrimState m) -> Double -> m Int
+experiment g r = experiment' 0
+  where
+    experiment' cnt = do
+        let newCnt = cnt+1
+        v <- uniform g
+        if v <= r
+          then pure newCnt
+          else experiment' newCnt
+
+analyzeResult :: [Int] -> IO ()
+analyzeResult [] = putStrLn "Result is empty."
+analyzeResult xs = do
+    let xMin = minimum xs
+        xMax = maximum xs
+
+    printf "min: %d, max: %d\n" xMin xMax
+
 main :: IO ()
 main = do
     [rawDropRate,rawCnt] <- getArgs
     case (,) <$> parseRate rawDropRate <*> parseCount rawCnt of
-        Just _ -> pure ()
+        Just (dropRate,cnt) -> do
+            printf "Performing %d experiments with drop rate %6.4f%%:\n" cnt (dropRate * 100)
+            g <- createSystemRandom
+            results <- replicateM cnt (experiment g dropRate)
+            analyzeResult results
         Nothing -> do
             putStrLn "Usage: <rate> <# of experiments>"
             putStrLn "example of rate: '0.20' or '20%'"
+
     pure ()
