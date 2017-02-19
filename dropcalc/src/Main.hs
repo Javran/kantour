0b1a72c44af25cc04f441f81b430fa1982c51f21
Javran Cheng <Javran.c@gmail.com>
2017-02-18 23:20:38 -0500

    some refactoring

--- dropcalc/src/Main.hs
+++ dropcalc/src/Main.hs
@@ -11,25 +11,6 @@ import Data.Vector.Algorithms.Intro
 import Data.Function
 import Numeric.SpecFunctions
 
--- either a number between 0 and 1
--- or {x}% where x can be between 0 and 100
-parseRate :: String -> Maybe Double
-parseRate [] = Nothing
-parseRate xs
-    | last xs == '%'
-    , [(r,"")] <- reads (init xs) = Just (r/100) >>= verify
-    | [(r,"")] <- reads xs = Just r >>= verify
-    | otherwise = Nothing
-  where
-    verify x = x <$ guard (x >= 0 && x <= 1)
-
--- must be positive number
-parseCount :: String -> Maybe Int
-parseCount xs
-    | [(v,"")] <- reads xs
-    , v > 0 = Just v
-    | otherwise = Nothing
-
 experiment :: PrimMonad m => Gen (PrimState m) -> Double -> m Int
 experiment g r = experiment' 0
   where
@@ -98,8 +79,24 @@ main = do
                 g <- createSystemRandom
                 results <- replicateM cnt (experiment g dropRate)
                 analyzeResult cnt dropRate results
-        _ -> printHelp
+        _ -> putStrLn "Usage: <rate> <# of experiments>"
+             >> putStrLn "example of rate: '0.20' or '20%'"
   where
-    printHelp = do
-        putStrLn "Usage: <rate> <# of experiments>"
-        putStrLn "example of rate: '0.20' or '20%'"
+    -- either a number between 0 and 1
+    -- or {x}% where x can be between 0 and 100
+    parseRate :: String -> Maybe Double
+    parseRate [] = Nothing
+    parseRate xs
+        | last xs == '%'
+        , [(r,"")] <- reads (init xs) = Just (r/100) >>= verify
+        | [(r,"")] <- reads xs = Just r >>= verify
+        | otherwise = Nothing
+      where
+        verify x = x <$ guard (x >= 0 && x <= 1)
+
+    -- must be positive number
+    parseCount :: String -> Maybe Int
+    parseCount xs
+        | [(v,"")] <- reads xs
+        , v > 0 = Just v
+        | otherwise = Nothing
