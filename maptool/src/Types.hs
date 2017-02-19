a8be18cc883c0136f13f907b64603b5aa1d7853d
Javran Cheng <Javran.c@gmail.com>
2017-02-18 23:33:47 -0500

    new structure for putting related info together

--- maptool/src/Types.hs
+++ maptool/src/Types.hs
@@ -3,10 +3,16 @@ module Types
   ( MyLine(..)
   , lName, lStart, lEnd
   , simpleLName
+
+  , MapInfo(..)
+  , getNodeName
+  , miLines, miNodeNames, miStarts -- TODO: this might be overkill...
   ) where
 
 import Linear
 import Control.Lens
+import qualified Data.Set as S
+import qualified Data.Map.Strict as M
 
 data MyLine = MyLine
   { _lName :: String
@@ -22,3 +28,16 @@ simpleLName l = case splitAt 4 n of
     _ -> n
   where
     n = _lName l
+
+data MapInfo = MapInfo
+  { _miLines :: [MyLine]
+  , _miStarts :: S.Set (V2 Int)
+  , _miNodeNames :: M.Map (V2 Int) String -- ^ INVARIANT: all nodes have a name in it
+  }
+
+makeLenses ''MapInfo
+
+getNodeName :: V2 Int -> MapInfo -> String
+getNodeName p MapInfo { _miNodeNames=mn }
+    | Just n <- M.lookup p mn = n
+    | otherwise = error $ "missing node name for " ++ show p
