62935d8ac32ee6757ed2bac2dded8a45c72bff65
Javran Cheng <Javran.c@gmail.com>
2017-02-23 23:16:10 -0500

    quick unionAL

--- src/Kantour/Utils.hs
+++ src/Kantour/Utils.hs
@@ -18,3 +18,23 @@ insertAL k v = alterAL (const (Just v)) k
 
 deleteAL :: Eq k => k -> [(k,v)] -> [(k,v)]
 deleteAL k = alterAL (const Nothing) k
+
+onePassInsert :: Eq k => [(k,v)] -> [(k,v)] -> ([(k,v)],[(k,v)])
+onePassInsert xs@((xk,xv):xs') ys@((yk,yv):ys')
+    | xk == yk =
+        let (result,todo) = onePassInsert xs' ys'
+        in ((yk,yv):result,todo)
+    | otherwise =
+        let (result,todo) = onePassInsert xs' ys
+        in ((xk,xv):result,todo)
+onePassInsert [] ys = ([],ys)
+onePassInsert xs [] = (xs,[])
+
+unionAL :: Eq k => [(k,v)] -> [(k,v)] -> [(k,v)]
+unionAL xs ys =
+    let (result,todo) = onePassInsert xs ys
+    in if null todo
+          then result
+          else if length ys == length todo
+                  then result ++ todo
+                  else unionAL result todo
