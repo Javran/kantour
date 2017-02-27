1fba09ee309d1c466bb1fd8df879e869e8445a1d
Javran Cheng <Javran.c@gmail.com>
2017-02-27 00:59:44 -0500

    an overcomplicated attempt of parsing text

--- src/Kantour/QuotesFetch/QParser.hs
+++ src/Kantour/QuotesFetch/QParser.hs
@@ -1,5 +1,7 @@
 module Kantour.QuotesFetch.QParser where
 
+import Data.List
+
 import Text.Megaparsec
 import Text.Megaparsec.String
 
@@ -43,6 +45,13 @@ that is: things like headers and templates can only appear right at the beginnin
 
 I think it's very promising that we don't need any backtracking at all.
 
+some component might appear in key-value part of a template:
+
+- begin with "[" => link
+- "<br>" / "<br />" / "<br/>"
+- "<ref> ... </ref>"
+- "{{lang ...}}
+
 -}
 
 data Header = Header
@@ -72,3 +81,53 @@ pTemplate = do
     tpArgs <- pArg `sepBy` (char '|' >> space) :: Parser [TemplateArg]
     _ <- string "}}"
     pure (Template tpName tpArgs)
+
+{-
+TODO: do we get a speed boost, if dlist, rather than [Char] is used?
+
+"pText cond" tries to consume as much input as possible, forcing the result to be text
+and stops when a requirement is met.
+
+this parser consumes at least one input upon success, and thus
+the first char in the input is *not* checked against "cond".
+-}
+pText :: (Char -> Bool) -> Parser String
+pText stopCond = do
+    x <- pElem <|> (pure <$> anyChar)
+    xs <- many (pElem <|> (pure <$> satisfy (not . stopCond)))
+    pure (concat (x : xs))
+  where
+    -- consumes one or more whitespaces.
+    -- returns a "\n" when the input begins with "\n\n"
+    -- otherwise a single " " is returned
+    pSpaces :: Parser String
+    pSpaces = do
+        sp <- some spaceChar
+        pure (if "\n\n" `isPrefixOf` sp
+                then "\n"
+                else " ")
+    -- "<br>" / "<br/>" / "<br   />"
+    pBr :: Parser String
+    pBr = "\n" <$
+        (string "<br"
+         >> space
+         >> (string "/>" <|> string ">"))
+    pRef :: Parser String
+    pRef = "" <$
+         (string "<ref>"
+          >> manyTill anyChar (string "</ref>"))
+    pLink :: Parser String
+    pLink = do
+        endP <- (string "]]" <$ string "[[")
+                <|> (string "]" <$ string "]")
+        linkContent <- manyTill anyChar endP
+        let content1 = dropWhile (/= '|') linkContent
+        case content1 of
+            [] -> pure linkContent -- pipe not found, return full content
+            '|':content2 -> pure content2 -- cut first part and return the rest of it
+            _ -> error "pText: pLink: unreachable"
+    -- TODO: for handling {{lang ...}} stuff.
+    pTemplateAsText :: Parser String
+    pTemplateAsText = undefined
+
+    pElem = pSpaces <|> pBr <|> pRef <|> pLink <|> pTemplateAsText
