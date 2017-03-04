eb8e2ba4cad606310391140c6977cd5b92b642a6
Javran Cheng <Javran.c@gmail.com>
2017-03-04 18:17:06 -0500

    regular and seasonal processing with log

--- src/Kantour/QuotesFetch/Main.hs
+++ src/Kantour/QuotesFetch/Main.hs
@@ -1,58 +1,47 @@
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ScopedTypeVariables, OverloadedStrings #-}
 module Kantour.QuotesFetch.Main where
 
 import Kantour.QuotesFetch.Fetch
 import Kantour.QuotesFetch.ShipDatabase
-import Kantour.QuotesFetch.Quotes
 import Kantour.QuotesFetch.Kcwiki
+import Kantour.QuotesFetch.PageProcessor
 import Kantour.QuotesFetch.ComponentParser
+import Kantour.QuotesFetch.PageParser
 
-import Text.PrettyPrint.HughesPJClass
-
-import qualified Kantour.QuotesFetch.PageParser as PP
 import Text.Megaparsec
 import Data.Coerce
 import Data.Maybe
-import Kantour.QuotesFetch.PageProcessor
-import Control.Monad.Logger
+
 import qualified Data.Text as T
+import qualified Data.IntMap as IM
+import Control.Monad.IO.Class
+import Control.Monad
+import Control.Monad.Logger
 
-{-# ANN module "HLint: ignore Avoid lambda" #-}
+{-# ANN module ("HLint: ignore Avoid lambda" :: String) #-}
 
-processRegularQuotes :: IO ()
-processRegularQuotes = do
+processAndCombine :: IO ()
+processAndCombine = do
     sdb <- getDatabase <$> fetchRawDatabase
-    rqs <- fetchRawQuotes
-    pages <- mapM (processPage sdb) rqs
-    mapM_ (\page -> print (renderAll sdb kc3Table page)) pages
-
-demoProcessRegularPage :: IO ()
-demoProcessRegularPage = do
-    content <- fetchWikiLink "天津风"
-    case parse PP.pScanAll "" content of
-        Left err -> print err
-        Right (Page result) -> do
-            let Just (trs,xs) = parseShipInfoPage (coerce result)
-            putStrLn (prettyShow trs)
-            let ppr (k,vs) = putStrLn (prettyShow k) >> putStrLn (prettyShow vs)
-            mapM_ ppr xs
-
-demoProcessSeasonalPage :: IO ()
-demoProcessSeasonalPage = do
-    sdb <- getDatabase <$> fetchRawDatabase
-    content <- fetchWikiLink "季节性/2017年节分季节"
-    let Right (Page result) = parse PP.pScanAll "" content
-        pageContent = fromJust (parseSeasonalPage result)
-    tbl <- runStdoutLoggingT (processSeasonal (T.pack "main") sdb pageContent)
-    pure ()
-
-dumpAllPages :: IO ()
-dumpAllPages = do
     result <- fetchWikiLink "Template:舰娘导航"
-    let (Right links) = parse PP.pCollectLinks "" result
-    contents <- unlines <$> mapM fetchWikiLink links
-    writeFile "packed.raw" contents
+    let (Right links) = parse pCollectLinks "" result
+        processLink link = do
+            content <- liftIO $ fetchWikiLink link
+            case parse pScanAll "" content of
+                Left err -> liftIO $ print err >> undefined
+                Right (Page pgResult) -> do
+                    let Just (trs,xs) = parseShipInfoPage (coerce pgResult)
+                    processRegular (T.pack link) sdb (trs,xs)
+    regulars <- runStdoutLoggingT $ do
+        tqss <- mapM processLink links
+        foldM (\acc i -> loggedSQTUnion "N/A" acc (IM.toList i)) IM.empty tqss
+
+    content' <- fetchWikiLink "季节性/2017年节分季节"
+    let Right (Page result') = parse pScanAll "" content'
+        pageContent = fromJust (parseSeasonalPage result')
+    seasonals <- runStdoutLoggingT (processSeasonal "main" sdb pageContent)
+    fin <- runStdoutLoggingT (loggedSQTUnion "N/A" regulars (IM.toList seasonals))
+    print (IM.size fin)
 
 defaultMain :: IO ()
-defaultMain = do
-    demoProcessSeasonalPage
+defaultMain = pure ()
--- src/Kantour/QuotesFetch/PageProcessor.hs
+++ src/Kantour/QuotesFetch/PageProcessor.hs
@@ -13,26 +13,25 @@ import qualified Data.Text as T
 import Data.Monoid
 
 {-# ANN module ("HLint: ignore Eta reduce" :: String) #-}
+{-# ANN module ("HLint: ignore Avoid lambda" :: String) #-}
+{-# ANN module ("HLint: ignore Use unless" :: String) #-}
 
-modifyWithLog :: Monad m
-                 => (Int -> Maybe v -> v -> m ())
-                 -> IM.IntMap v -> Int -> (Maybe v -> m v) -> m (IM.IntMap v)
+modifyWithLog ::
+    Monad m
+    => (Int -> Maybe v -> v -> m ())
+    -> IM.IntMap v -> Int -> (Maybe v -> m v) -> m (IM.IntMap v)
 modifyWithLog doLog ms k modifyM = do
     let mOldV = IM.lookup k ms
     newV <- modifyM mOldV
     doLog k mOldV newV
     pure (IM.insert k newV ms)
 
-processSeasonal :: forall m. MonadLogger m
-                   => T.Text -> ShipDatabase -> PageContent 'Seasonal -> m ShipQuoteTable
-processSeasonal src sdb qs = foldM go IM.empty (process <$> qs)
+-- SQT: ShipQuoteTable
+loggedSQTInsert ::
+    forall m. MonadLogger m
+    => T.Text -> ShipQuoteTable -> [(MasterId,(Int, QuoteLine))] -> m ShipQuoteTable
+loggedSQTInsert src initSqt xs = foldM go initSqt xs
   where
-    findMst = qlFindMasterId sdb . qlArchive
-    findSituation = qaSituationId . qlArchive
-
-    process :: QuoteLine -> (MasterId, (Int, QuoteLine))
-    process = findMst &&& (findSituation &&& id)
-
     go :: ShipQuoteTable -> (MasterId, (Int, QuoteLine)) -> m ShipQuoteTable
     go cur (mstId,(sId,ql)) =
         modifyWithLog doLog cur mstId modifyQuoteTable
@@ -44,20 +43,88 @@ processSeasonal src sdb qs = foldM go IM.empty (process <$> qs)
         modifyQuoteTable :: Maybe (IM.IntMap QuoteLine) -> m (IM.IntMap QuoteLine)
         modifyQuoteTable mOldTbl = modifyWithLog doLog2 curTbl sId (const (pure ql))
           where
-            isSeasonal = qaIsNormalSeasonal . qlArchive
-            doLog2 _ Nothing _ =
-                pure ()
-                -- logInfoNS src $ "inserted: " <> T.pack (show sId)
-            doLog2 _ (Just oldQl) newQl = log' src msg
-              where
-                msg = "mstId=" <> T.pack (show mstId)
-                      <> ", sId=" <> T.pack (show sId)
-                      <> " replacing " <> getDesc oldQl <> " with " <> getDesc newQl
-                log' = if not (isSeasonal oldQl) && isSeasonal newQl
-                         then logInfoNS else logWarnNS
-                getDesc x =  if isSeasonal x then "seasonal" else "normal"
+            doLog2 = quoteLineReplacingLog src mstId
             curTbl = fromMaybe IM.empty mOldTbl
+
+processSeasonal ::
+    forall m. MonadLogger m
+    => T.Text -> ShipDatabase -> PageContent 'Seasonal -> m ShipQuoteTable
+processSeasonal src sdb qs =
+    loggedSQTInsert src IM.empty (indexedQuoteLine sdb <$> qs)
+
+indexedQuoteLine :: ShipDatabase -> QuoteLine -> (MasterId, (Int, QuoteLine))
+indexedQuoteLine sdb = findMst &&& (findSituation &&& id)
+  where
+    findMst = qlFindMasterId sdb . qlArchive
+    findSituation = qaSituationId . qlArchive
 
 qlFindMasterId :: ShipDatabase -> QuoteArchive -> MasterId
 qlFindMasterId _ QARaw{} = error "qlFindMasterId: unexpected QARaw"
 qlFindMasterId sdb (QANormal lId _ _) = findMasterId lId sdb
+
+processRegular ::
+    forall m. MonadLogger m
+    => T.Text -> ShipDatabase -> PageContent 'ShipInfo -> m ShipQuoteTable
+processRegular src sdb (TR tbs,sectionsRaw) = do
+    -- select section that has contents in it
+    let sections = filter valid sectionsRaw
+          where
+            valid (Header _ secName,xs) = not (null xs) && secName /= "语音存档"
+        mstIdTable = (map . second) (\lId -> findMasterId lId sdb) tbs
+        processSection :: (Header, [QuoteLine]) -> m [(MasterId,(Int,QuoteLine))]
+        processSection (Header _ secName, qls) = do
+            when (secName `notElem` ["报时","时报"]) $ do
+                let mMstId = lookup secName mstIdTable
+                case mMstId of
+                    Nothing ->
+                        logWarnNS src
+                        $ "section '" <> T.pack secName <> "' not found in tabber"
+                    Just mstId -> do
+                        let qMstIds = qlFindMasterId sdb . qlArchive <$> qls
+                            inconsistents = filter (/= mstId) qMstIds
+                        when (not (null inconsistents)) $
+                          logWarnNS src $ "found inconsistent mst ids: "
+                          <> T.pack (show inconsistents)
+                          <> " under section '" <> T.pack secName <> "'"
+            pure (indexedQuoteLine sdb <$> qls)
+    xs <- concat <$> mapM processSection sections
+    loggedSQTInsert src IM.empty xs
+
+-- SQT: ShipQuoteTable
+loggedSQTUnion ::
+    forall m. MonadLogger m
+    => T.Text -> ShipQuoteTable -> [(MasterId, IM.IntMap QuoteLine)] -> m ShipQuoteTable
+loggedSQTUnion src initSqt xs = foldM go initSqt xs
+  where
+    go :: ShipQuoteTable -> (MasterId, IM.IntMap QuoteLine) -> m ShipQuoteTable
+    go cur (mstId,subM) =
+        modifyWithLog doLog cur mstId modifyQuoteTable
+      where
+        doLog _ _ _ =
+            pure ()
+            -- logInfoNS src $ "created: " <> T.pack (show mstId)
+
+        sPairs = IM.toList subM
+        modifyQuoteTable :: Maybe (IM.IntMap QuoteLine) -> m (IM.IntMap QuoteLine)
+        modifyQuoteTable mOldTbl = foldM go2 curTbl sPairs
+          where
+            curTbl = fromMaybe IM.empty mOldTbl
+            go2 :: IM.IntMap QuoteLine -> (Int, QuoteLine) -> m (IM.IntMap QuoteLine)
+            go2 curTbl' (sId,ql) = modifyWithLog doLog2 curTbl' sId (const (pure ql))
+              where
+                doLog2 = quoteLineReplacingLog src mstId
+
+quoteLineReplacingLog ::
+    MonadLogger m
+    => T.Text -> MasterId -> Int -> Maybe QuoteLine -> QuoteLine -> m ()
+quoteLineReplacingLog _ _ _ Nothing _ = pure ()
+    -- logInfoNS src $ "inserted: " <> T.pack (show sId)
+quoteLineReplacingLog src mstId sId (Just oldQl) newQl = log' src msg
+  where
+    isSeasonal = qaIsNormalSeasonal . qlArchive
+    msg = "mstId=" <> T.pack (show mstId)
+          <> ", sId=" <> T.pack (show sId)
+          <> " replacing " <> getDesc oldQl <> " with " <> getDesc newQl
+    log' = if not (isSeasonal oldQl) && isSeasonal newQl
+             then logInfoNS else logWarnNS
+    getDesc x =  if isSeasonal x then "seasonal" else "normal"
