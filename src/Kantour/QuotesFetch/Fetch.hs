060976b2f9638c37fcf6386527c153612ab8b74b
Javran Cheng <Javran.c@gmail.com>
2017-03-04 23:27:13 -0500

    remove old parser impl

--- src/Kantour/QuotesFetch/Fetch.hs
+++ src/Kantour/QuotesFetch/Fetch.hs
@@ -10,12 +10,6 @@ import Network.HTTP.Client
 import Network.HTTP.Client.TLS
 import Network.HTTP.Types
 import Data.ByteString.Builder
-import Data.List
-import Control.Monad
-
-import Control.Concurrent.ParallelIO.Global
-import Kantour.QuotesFetch.Parser
-import Kantour.QuotesFetch.Types
 
 endpoint :: String
 endpoint = "http://zh.kcwiki.moe/api.php"
@@ -53,31 +47,5 @@ fetchWikiLink wlink = do
              >>> deep (hasName "revisions" /> hasName "rev")
              >>> getChildren) raw
 
-fetchRawQuotesByLink :: String
-                     -> IO (LinkName, Maybe RawPage)
-fetchRawQuotesByLink linkName = do
-    content <- fetchWikiLink linkName
-    pure . (linkName,) $ do
-        guard $ "==舰娘属性==" `isInfixOf` content
-        let page = collectAll content
-        pure page
-
-fetchRawQuotes :: IO [(LinkName, RawPage)]
-fetchRawQuotes = do
-    resp <- fetchWikiLink "Template:舰娘导航"
-    let links = filter (not . notKanmusuLink) (extractLinks resp)
-        -- testLinks = take 5 links
-        testLinks = links
-    results <- parallel (map fetchRawQuotesByLink testLinks)
-    stopGlobalPool
-    let (ls,rs) =
-            partitionEither $
-                map (\(l,m) -> maybe (Left l) (Right . (l,)) m)
-                    results
-    putStrLn "Fetched nothing from following links:"
-    putStrLn (intercalate ", " ls)
-    pure rs
-
 fetchRawDatabase :: IO String
 fetchRawDatabase = fetchWikiLink "模块:舰娘数据"
-
