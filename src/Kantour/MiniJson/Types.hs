09edb6027e83716aed2b68586976e250c83bdda1
Javran Cheng <Javran.c@gmail.com>
2017-03-14 01:25:00 -0400

    JValue pretty printing

--- src/Kantour/MiniJson/Types.hs
+++ src/Kantour/MiniJson/Types.hs
@@ -1,6 +1,8 @@
 module Kantour.MiniJson.Types where
 
 import qualified Data.Text as T
+import Text.PrettyPrint
+import Text.PrettyPrint.HughesPJClass
 
 -- INVARIANT: all appearance of Integer below should all be non-negative
 data JValue
@@ -16,3 +18,21 @@ data JValue
   | JBool Bool
   | JNull
     deriving (Show)
+
+instance Pretty JValue where
+    pPrint JNull = text "null"
+    pPrint (JBool b) = text $ if b then "true" else "false"
+    pPrint (JText t) = text $ T.unpack t
+    pPrint (JArray xs) = brackets . fsep . punctuate comma . map pPrint $ xs
+    pPrint (JNum n bd mad mes) = pNeg <> pBefore <> pAfter <> pEs
+      where
+        pNeg = if n then char '-' else empty
+        pBefore = integer bd
+        pAfter = maybe empty (\ad -> char '.' <> integer ad) mad
+        pEs = maybe empty (\(eNeg,ePart) ->
+                           let pN' = if eNeg then char '-' else empty
+                           in text "e" <> pN' <> integer ePart) mes
+    pPrint (JObject xs) = brackets . fsep . punctuate comma . map pPair $ xs
+      where
+        pPair :: (T.Text, JValue) -> Doc
+        pPair (k,v) = parens (text (T.unpack k) <> comma <> pPrint v)
