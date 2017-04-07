6025967c22b18edc331c4d28e0f9a7eb0b6a6277
Javran Cheng <Javran.c@gmail.com>
2017-04-06 23:50:19 -0400

    EquipmentInfo first def

---
+++ src/Kantour/Kcwiki/EquipmentInfo.hs
@@ -0,0 +1,17 @@
+module Kantour.Kcwiki.EquipmentInfo where
+
+-- TODO: getting complicated here, might need to wait for a while
+
+data EquipmentInfo = EquipmentInfo
+  { masterId :: Int
+  , nameJP :: String
+  , nameCN :: String
+  , category :: ()
+  , rarity :: Int
+  , status :: ()
+  , stats :: ()
+  , dismantle :: ()
+  , compatibleTypes :: ()
+  , extra :: ()
+  }
+
