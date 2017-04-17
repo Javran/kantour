71d46a8d1253faef8f699224be14cd344966e271
Javran Cheng <Javran.c@gmail.com>
2017-04-16 23:41:14 -0400

    split lazy bytestring into lines

--- src/Kantour/WhoCallsTheFleet/Main.hs
+++ src/Kantour/WhoCallsTheFleet/Main.hs
@@ -4,34 +4,46 @@ module Kantour.WhoCallsTheFleet.Main where
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import qualified Data.ByteString.Lazy.Char8 as LBS
+import qualified Data.ByteString as BS
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
 import Network.HTTP.Types
 import Data.Aeson
 
 import GHC.Generics
+import Data.String
 
 repoBase :: String
 repoBase = "https://raw.githubusercontent.com/Diablohu/WhoCallsTheFleet/master/app-db/"
 
-fetchURL :: String -> IO String
+fetchURL :: String -> IO LBS.ByteString
 fetchURL url = do
     mgr <- getGlobalManager
     req <- parseRequest url
     resp <- httpLbs req mgr
     let st = responseStatus resp
     if st == ok200
-        then pure (T.unpack . T.decodeUtf8 . LBS.toStrict . responseBody $ resp)
+        then pure (responseBody resp)
         else fail $ "error with status code: " ++ show (statusCode st)
 
-fetchShipsRaw :: IO String
-fetchShipsRaw = fetchURL (repoBase ++ "ships.json")
+splitLines :: LBS.ByteString -> [LBS.ByteString]
+splitLines xs
+    | LBS.null xs = []
+    | (y,ys) <- LBS.span (/= '\n') xs = y : splitLines (LBS.drop 1 ys)
 
+fetchShipsRaw :: IO LBS.ByteString
+fetchShipsRaw = fetchURL (repoBase ++ "ships.json")
+{-
 fetchItemsRaw :: IO String
-fetchItemsRaw = fetchURL (repoBase ++ "items.json")
+fetchItemsRaw = fetchURL (repoBase ++ "items.json") -}
+
+parseShip :: String -> Maybe Ship
+parseShip xs = decode (fromString xs)
 
 defaultMain :: IO ()
-defaultMain = fetchShipsRaw >>= putStrLn
+defaultMain = do
+    raw <- fetchShipsRaw
+    print (splitLines raw)
 
 data Equipment = Equipment
   { eqpId :: Int
@@ -47,7 +59,7 @@ data Equipment = Equipment
 
 data Ship = Ship
   { shipId :: Int
-  } deriving Generic
+  } deriving (Generic, Show)
 
 instance FromJSON Ship where
     parseJSON = withObject "Ship" $ \v -> Ship
