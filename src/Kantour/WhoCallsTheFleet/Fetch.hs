ffb0223e72894ca53aea6c2688cda4ccb786e3ee
Javran Cheng <Javran.c@gmail.com>
2017-04-21 23:53:16 -0400

    fetching items, moving some code around

--- src/Kantour/WhoCallsTheFleet/Fetch.hs
+++ src/Kantour/WhoCallsTheFleet/Fetch.hs
@@ -1,9 +1,24 @@
 module Kantour.WhoCallsTheFleet.Fetch where
 
-import qualified Data.ByteString.Lazy.Char8 as LBS
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
 import Network.HTTP.Types
+
+import qualified Data.ByteString.Lazy.Char8 as LBS
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BSC
+
+import Data.Word
+import Data.Char
+import Data.MonoTraversable
+
+w2c :: Word8 -> Char
+w2c = chr . fromIntegral
+
+splitLines :: LBS.ByteString -> [BS.ByteString]
+splitLines = filter nonEmptyLine . BSC.split '\n' . LBS.toStrict
+  where
+    nonEmptyLine = oany (not . isSpace . w2c)
 
 repoBase :: String
 repoBase = "https://raw.githubusercontent.com/Diablohu/KanColle-JSON-Database/master/db/"
@@ -18,5 +33,8 @@ fetchURL url = do
         then pure (responseBody resp)
         else fail $ "error with status code: " ++ show (statusCode st)
 
-fetchShipsRaw :: IO LBS.ByteString
-fetchShipsRaw = fetchURL (repoBase ++ "ships.json")
+fetchShipsRaw :: IO [BS.ByteString]
+fetchShipsRaw = splitLines <$> fetchURL (repoBase ++ "ships.json")
+
+fetchEquipmentsRaw :: IO [BS.ByteString]
+fetchEquipmentsRaw = splitLines <$> fetchURL (repoBase ++ "items.json")
--- src/Kantour/WhoCallsTheFleet/Main.hs
+++ src/Kantour/WhoCallsTheFleet/Main.hs
@@ -1,36 +1,23 @@
 {-# LANGUAGE OverloadedStrings, TypeFamilies, DuplicateRecordFields #-}
 module Kantour.WhoCallsTheFleet.Main where
 
-import qualified Data.ByteString.Lazy.Char8 as LBS
-import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as BSC
 import Data.Aeson
 
-import Data.Char
-import Data.MonoTraversable
 import Data.Semigroup
 import Data.String
-import Data.Word
 import Control.Monad
 import Data.Maybe
 import Kantour.WhoCallsTheFleet.Types
 
 import Kantour.WhoCallsTheFleet.Fetch (fetchShipsRaw)
 
-w2c :: Word8 -> Char
-w2c = chr . fromIntegral
-
-splitLines :: LBS.ByteString -> [BS.ByteString]
-splitLines = filter nonEmptyLine . BSC.split '\n' . LBS.toStrict
-  where
-    nonEmptyLine = oany (not . isSpace . w2c)
-
 parseShip :: String -> Maybe Ship
 parseShip xs = decode (fromString xs)
 
 defaultMain :: IO ()
 defaultMain = do
-    raws <- splitLines <$> fetchShipsRaw
+    raws <- fetchShipsRaw
     let process :: BSC.ByteString -> IO ()
         process raw = do
             let result = eitherDecodeStrict' raw :: Either String Ship
