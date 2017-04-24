5f1cb3342d8789465c01a0d4638294f651e5f2fb
Javran Cheng <Javran.c@gmail.com>
2017-04-23 23:01:41 -0400

    equipment name

--- src/Kantour/WhoCallsTheFleet/Types/Equipment.hs
+++ src/Kantour/WhoCallsTheFleet/Types/Equipment.hs
@@ -3,12 +3,13 @@ module Kantour.WhoCallsTheFleet.Types.Equipment where
 
 import GHC.Generics
 import Data.Aeson
+import qualified Data.Text as T
 
 data Equipment = Equipment
   { masterId :: Int
   , rarity :: Int
   , eqpType :: Int
-  , name :: () -- TODO
+  , name :: EquipmentName
   , stat :: ()
   , dismantle :: ()
   , craftable :: Bool
@@ -16,14 +17,28 @@ data Equipment = Equipment
   , rankUpgradable :: ()
   } deriving (Generic, Show)
 
+data EquipmentName = EquipmentName
+  { jaJP :: T.Text
+  , jaKana :: T.Text
+  , jaRomaji :: T.Text
+  , zhCN :: T.Text
+  } deriving (Generic, Show)
+
 instance FromJSON Equipment where
     parseJSON = withObject "Equipment" $ \v -> Equipment
         <$> v .: "id"
         <*> v .: "rarity"
         <*> v .: "type"
-        <*> pure ()
+        <*> v .: "name"
         <*> pure ()
         <*> pure ()
         <*> v .: "craftable"
         <*> v .: "improvable"
         <*> pure ()
+
+instance FromJSON EquipmentName where
+    parseJSON = withObject "EquipmentName" $ \v -> EquipmentName
+        <$> v .: "ja_jp"
+        <*> v .: "ja_kana"
+        <*> v .: "ja_romaji"
+        <*> v .: "zh_cn"
