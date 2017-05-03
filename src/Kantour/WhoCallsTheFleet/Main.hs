d283d25e643fea9ffc97d7441f7cc4f4ab6052f1
Javran Cheng <Javran.c@gmail.com>
2017-05-02 23:41:50 -0400

    remove unused imports
    Signed-off-by: Javran Cheng <Javran.c@gmail.com>

--- src/Kantour/WhoCallsTheFleet/Main.hs
+++ src/Kantour/WhoCallsTheFleet/Main.hs
@@ -1,14 +1,7 @@
 {-# LANGUAGE OverloadedStrings, TypeFamilies, DuplicateRecordFields #-}
 module Kantour.WhoCallsTheFleet.Main where
 
-import Data.Aeson
-
-import Data.String
-import Kantour.WhoCallsTheFleet.Types
 import Kantour.WhoCallsTheFleet.Fetch
-
-parseShip :: String -> Maybe Ship
-parseShip xs = decode (fromString xs)
 
 defaultMain :: IO ()
 defaultMain = do
