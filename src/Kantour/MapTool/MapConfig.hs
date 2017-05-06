4a4ce339e3abdd889884976dd593d6121efcd680
Javran Cheng <Javran.c@gmail.com>
2017-05-05 23:10:41 -0400

    MapConfig definition

---
+++ src/Kantour/MapTool/MapConfig.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE DuplicateRecordFields, OverloadedStrings #-}
+module Kantour.MapTool.MapConfig where
+
+import Data.Aeson
+import Data.Aeson.Types
+
+data MapConfig = MapConfig
+  { sourceFile :: FilePath
+  , extraSources :: [MapExtraSource]
+  }
+
+data MapExtraSource = MapExtraSource
+  { sourceFile :: FilePath
+  , matchString :: String
+  }
+
+instance FromJSON MapConfig where
+    parseJSON = withObject "MapConfig" $ \v -> MapConfig
+        <$> v .: "source"
+        <*> v .: "extras"
+
+instance FromJSON MapExtraSource where
+    parseJSON = withObject "MapExtraSource" $ \v -> MapExtraSource
+        <$> v .: "source"
+        <*> v .: "match-string"
