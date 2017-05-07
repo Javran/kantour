c609c6ed7a0bd55f238ef290ec05a0a500720f38
Javran Cheng <Javran.c@gmail.com>
2017-05-06 23:16:51 -0400

    new module for estimating ship stats

---
+++ mains/shipstat/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import Kantour.ShipStat.Main
+
+main :: IO ()
+main = defaultMain
---
+++ src/Kantour/ShipStat/Main.hs
@@ -0,0 +1,11 @@
+module Kantour.ShipStat.Main where
+
+{-
+
+this module is for estimating several ship stats: ASW, LoS and Evasion,
+all of them depends on the level and few other hidden stats and is computed the same way.
+
+-}
+
+defaultMain :: IO ()
+defaultMain = pure ()
