2e76413e43f9c853e33fd79896b1e03142cb5eaa
Javran Cheng <Javran.c@gmail.com>
2017-05-06 23:40:59 -0400

    data file parsing

--- src/Kantour/ShipStat/Main.hs
+++ src/Kantour/ShipStat/Main.hs
@@ -1,5 +1,26 @@
 module Kantour.ShipStat.Main where
 
+import System.Environment
+import qualified Data.IntMap as IM
+import Data.Maybe
+import Control.Monad
+import Data.Char
+
+processRaw :: String -> IM.IntMap Int
+processRaw =
+      IM.fromList
+    . mapMaybe getDataLine
+    . lines
+  where
+    getDataLine :: String -> Maybe (Int,Int)
+    getDataLine raw = do
+        -- has non-empty content
+        guard (any (not . isSpace) raw)
+        -- not a comment line
+        guard (head raw /= '#')
+        case words raw of
+            [a,b] -> Just (read a, read b)
+            _ -> Nothing
 {-
 
 this module is for estimating several ship stats: ASW, LoS and Evasion,
@@ -8,4 +29,12 @@ all of them depends on the level and few other hidden stats and is computed the 
 -}
 
 defaultMain :: IO ()
-defaultMain = pure ()
+defaultMain = do
+    as <- getArgs
+    case as of
+        [fp] -> do
+            d <- processRaw <$> readFile fp
+            print d
+            pure ()
+        _ ->
+            putStrLn "shipstat <stat file>"
