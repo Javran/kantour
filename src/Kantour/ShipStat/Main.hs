f2d99b4404c4c8d8e744f4dad16ab1cf18d20200
Javran Cheng <Javran.c@gmail.com>
2017-05-07 23:23:22 -0400

    fix impl of shipstat

--- src/Kantour/ShipStat/Main.hs
+++ src/Kantour/ShipStat/Main.hs
@@ -16,7 +16,7 @@ getStat baseSt maxSt level = baseSt + floor lvlBonus
 
 findRange :: IM.IntMap Int -> ((Int, Int), (Int, Int))
 findRange m =
-    ( (minimum lv1Stats,maximum lv1Stats)
+    ( (minimum lv1Stats, maximum lv1Stats)
     , (minimum lv99Stats, maximum lv99Stats)
     )
   where
@@ -30,7 +30,7 @@ findRange m =
     estimatedRanges = do
         minStat' <- [minStat-1,minStat+1]
         maxStat' <- [maxStat-1,maxStat+1]
-        pure (estimate' (minLvl,maxLvl) (minStat',maxStat'))
+        pure (estimate' (minLvl,minStat') (maxLvl,maxStat'))
 
     lv1Stats = map fst estimatedRanges
     lv99Stats = map snd estimatedRanges
@@ -39,22 +39,23 @@ genRange :: ((Int,Int),(Int,Int)) -> [(Int, Int)]
 genRange ((lv1A,lv1B), (lv99A,lv99B)) =
     [ (lv1,lv99)
     | lv1 <- [lv1A ..lv1B]
-    , lv99 <- [lv99A,lv99B]
+    , lv99 <- [lv99A..lv99B]
     ]
 
 -- gives a value on Lv. 1 and Lv. 99
+-- TODO: not sure whether it's Lv. 1 or 0 considered base stat
 estimate' :: (Int, Int) -> (Int, Int) -> (Int, Int)
-estimate' (minX, maxX) (minY, maxY) = (floor (le 1), ceiling (le 99))
+estimate' (x1, y1) (x2, y2) = (floor (le 1), ceiling (le 99))
   where
-    le = linearEstimate (minX, maxX) (minY, maxY)
+    le = linearEstimate (x1, y1) (x2, y2)
 
 linearEstimate :: (Int, Int) -> (Int, Int) -> Int -> Double
-linearEstimate (minX, maxX) (minY, maxY) x = fI x * k + b
+linearEstimate (x1, y1) (x2, y2) x = fI x * k + b
   where
     fI :: Integral i => i -> Double
     fI = fromIntegral
-    k = fI (maxY - minY) / fI (maxX - minX)
-    b = fI minY - k * fI minX
+    k = fI (y2 - y1) / fI (x2 - x1)
+    b = fI y1 - k * fI x1
 
 processRaw :: String -> IM.IntMap Int
 processRaw =
@@ -81,14 +82,22 @@ all of them depends on the level and few other hidden stats and is computed the 
 
 -}
 
+checkTruth :: (Int, Int) -> [(Int,Int)] -> [(Int,Int)]
+checkTruth (lvl,stat) = filter check
+  where
+    check :: (Int, Int) -> Bool
+    check (baseStat, maxStat) = getStat baseStat maxStat lvl == stat
+
 defaultMain :: IO ()
 defaultMain = do
     as <- getArgs
     case as of
         [fp] -> do
             d <- processRaw <$> readFile fp
-            print d
-            print (findRange d)
+            let searchSpace = genRange (findRange d)
+                results = foldl (\curSearchSpace truth -> checkTruth truth curSearchSpace) searchSpace (IM.toList d)
+            -- print searchSpace
+            print results
             pure ()
         _ ->
             error "shipstat <stat file>"
