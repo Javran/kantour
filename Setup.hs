6c6da0c646152868d856451dca1beaad8e99d474
Javran Cheng <Javran.c@gmail.com>
2017-08-14 19:08:24 -0400

    reverting to Simple

--- Setup.hs
+++ Setup.hs
@@ -1,9 +1,4 @@
-{-# LANGUAGE NamedFieldPuns #-}
 import Distribution.Simple
-import Distribution.Simple.LocalBuildInfo
 
 main :: IO ()
-main = defaultMainWithHooks (simpleUserHooks {postBuild})
-  where
-    -- TODO
-    postBuild _ _ _ LocalBuildInfo {buildDir} = pure ()
+main = defaultMain
