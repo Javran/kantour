cfd853cf8526253e9f0a9a299df1e612bbe05265
Javran Cheng <Javran.c@gmail.com>
2017-11-09 22:48:57 -0500

    just plan layout

--- src/Kantour/ESports/Main.hs
+++ src/Kantour/ESports/Main.hs
@@ -10,3 +10,23 @@ instance Subcommand SubCmdESports where
 
 defaultMain :: IO ()
 defaultMain = pure ()
+
+{-
+
+  (TODO) plan:
+
+  - Make `QuestState`, which keeps track of completed, ongoing quests,
+    ensure that quest dependencies are taking into account properly
+
+  - Use progressors to make progress until we can do nothing more
+
+    - some optimization could help, for example:
+
+      - exclude those that does not make any progress on ongoing quests
+      - not sure how effective this would be, but it might be a good idea
+        to further exclude those that obviously less efficient than others
+        when the set of ongoing quests are taken into account
+  - "String" could be inefficient, what we need are just Int for
+    fast comparison, might worry about this later
+
+-}
