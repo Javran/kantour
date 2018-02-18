1668c754ec2d8365984d8ad8d98a312b9ea5f9a8
Javran Cheng <Javran.c@gmail.com>
2018-02-17 22:31:11 -0500

    map redraw tool

--- src/Kantour/MapRedraw.hs
+++ src/Kantour/MapRedraw.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ScopedTypeVariables, OverloadedStrings #-}
 module Kantour.MapRedraw where
 
 import System.Environment
@@ -6,12 +6,47 @@ import System.Exit
 import Kantour.Subcommand
 import Kantour.MapTool.Types
 import Kantour.MapTool.Draw
+import Data.String
+import qualified Data.Set as S
+import Linear
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.Map.Strict as M
+
+import Data.Aeson
+import Data.Aeson.Encode.Pretty
 
 data SubCmdMapRedraw
 
 instance Subcommand SubCmdMapRedraw where
     name _ = "MapRedraw"
     main _ = defaultMain
+
+newtype PoiFcdMapInfo = PoiMI MapInfo
+
+instance ToJSON PoiFcdMapInfo where
+    toJSON (PoiMI mi) = object ["route" .= route , "spots" .= spots]
+      where
+        ls = _miLines mi
+        starts = _miStarts mi
+        nodes = _miNodeNames mi
+        route :: Value
+        route = object $ ("0" .= [Null, "1"]) : fmap tr ls
+          where
+            tr (MyLine nm nStart nEnd) = fromString (cut nm) .= fmap nodeToName [nStart, nEnd]
+            nodeToName :: V2 Int -> Value
+            nodeToName coord
+                | Just nName <- M.lookup coord nodes = fromString nName
+                | otherwise = Null
+            cut :: String -> String
+            cut xs
+                | ("line", ys) <- splitAt 4 xs = ys
+                | otherwise = error "prefix 'line' expected"
+
+        spots = object $ fmap tr (M.toList nodes)
+          where
+            tr (coord@(V2 x y), n) = fromString n .= [toJSON x, toJSON y, mayStart]
+              where
+                mayStart = if coord `S.member` starts then "start" else ""
 
 {-
 
@@ -26,14 +61,17 @@ defaultMain :: IO ()
 defaultMain = do
     args <- getArgs
     let helpAndExit = do
-            putStrLn "MapRedraw <mapInfo> <output image>"
+            putStrLn "MapRedraw <mapInfo> <output image> <output poifcd>"
             exitSuccess
     case args of
         [] -> helpAndExit
-        [miFilePath, outFilePath] -> do
+        [miFilePath, outFilePath, outFcdPath] -> do
             raw <- readFile miFilePath
             case reads raw of
-                [(mapInfo :: MapInfo, _)] ->
+                [(mapInfo :: MapInfo, _)] -> do
                       withArgs (words "-w 800 -h 400 -o" ++ [outFilePath]) $ draw mapInfo
+                      let encoded = encodePretty' config (PoiMI mapInfo)
+                          config = defConfig {confCompare = compare}
+                      LBS.writeFile outFcdPath encoded
                 _ -> putStrLn "failed to parse the content"
         _ -> helpAndExit
