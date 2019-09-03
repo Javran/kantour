744797d10689d0f8ba3db13053d87d558c401598
Javran Cheng <Javran.c@gmail.com>
2019-09-02 22:27:25 -0700

    cracking magic code.

--- src/Kantour/GameResource/Magic.hs
+++ src/Kantour/GameResource/Magic.hs
@@ -1,10 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Kantour.GameResource.Magic
   ( servers
+  , magicCode
   ) where
 
-import qualified Data.Text as T
+import Data.Monoid
+import Data.Char
+
 import qualified Data.IntMap.Strict as IM
+import qualified Data.Text as T
+import qualified Data.Vector.Unboxed as VU
 
 servers :: IM.IntMap T.Text
 servers =
@@ -30,3 +35,26 @@ servers =
     , (19, "203.104.209.55")
     , (20, "203.104.209.102")
     ]
+
+resourceMagic :: VU.Vector Int
+resourceMagic = VU.fromList
+  [ 6657, 5699, 3371, 8909, 7719, 6229, 5449, 8561, 2987, 5501
+  , 3127, 9319, 4365, 9811, 9927, 2423, 3439, 1865, 5925, 4409
+  , 5509, 1517, 9695, 9255, 5325, 3691, 5519, 6949, 5607, 9539
+  , 4133, 7795, 5465, 2659, 6381, 6875, 4019, 9195, 5645, 2887
+  , 1213, 1815, 8671, 3015, 3147, 2991, 7977, 7045, 1619, 7909
+  , 4451, 6573, 4545, 8251, 5983, 2849, 7249, 7449, 9477, 5963
+  , 2711, 9019, 7375, 2201, 5631, 4893, 7653, 3719, 8819, 5839
+  , 1853, 9843, 9119, 7023, 5681, 2345, 9873, 6349, 9315, 3795
+  , 9737, 4633, 4173, 7549, 7171, 6147, 4723, 5039, 2723, 7815
+  , 6201, 5999, 5339, 4431, 2911, 4435, 3611, 4423, 9517, 3243
+  ]
+
+magicCode :: Int -> String -> Int
+magicCode r seed =
+    (17*(r+7) * (resourceMagic VU.! ((s+r*a) `rem` 100))) `rem` 8973 + 1000
+  where
+    s = getSum $ foldMap (Sum . ord) seed
+    a = case seed of
+      [] -> 1
+      _ -> length seed
