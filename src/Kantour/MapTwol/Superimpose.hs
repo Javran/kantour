e88f4ad8af6c028d9c07db9c97833521256e2d05
Javran Cheng <Javran.C@gmail.com>
2021-05-23 16:20:14 -0700

    a superimpose that supports alpha.

---
+++ src/Kantour/MapTwol/Superimpose.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE FlexibleContexts #-}
+
+module Kantour.MapTwol.Superimpose where
+
+import Graphics.Image
+import Graphics.Image.Interface
+
+-- https://github.com/lehins/hip/issues/33
+superimpose'
+  :: ( Array arr RGBA e
+     , AlphaSpace RGBA e
+     , Fractional e
+     , Ord e
+     )
+  => (Int, Int) -- ^ @(i, j)@ starting index from within a source image.
+  -> Image arr RGBA e -- ^ Image to be positioned above the source image.
+  -> Image arr RGBA e -- ^ Source image.
+  -> Image arr RGBA e
+superimpose' (!i0, !j0) !imgA !imgB = traverse2 imgB imgA const newPx
+  where
+    !(m, n) = dims imgA
+    newPx getPxB getPxA (i, j) =
+      let !(i', j') = (i - i0, j - j0)
+          new = getPxA (i', j')
+          old = getPxB (i, j)
+       in if i' >= 0 && j' >= 0 && i' < m && j' < n then overlayAlpha old new else old
+
+-- https://en.wikipedia.org/wiki/Alpha_compositing
+overlayAlpha
+  :: (Elevator e, Ord e, Fractional e)
+  => Pixel RGBA e
+  -> Pixel RGBA e
+  -> Pixel RGBA e
+overlayAlpha bPxa oPxa =
+  addAlpha rAlp (liftPx2 f (dropAlpha bPxa) (dropAlpha oPxa))
+  where
+    bAlp = getAlpha bPxa
+    oAlp = getAlpha oPxa
+    rAlp = oAlp + bAlp * (1 - oAlp)
+    f b o =
+      if rAlp == 0
+        then 0
+        else (o * oAlp + b * bAlp * (1 - oAlp)) / rAlp
