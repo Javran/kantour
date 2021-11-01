aed89f96da4679b8df541372e6570d18cd517a24
Javran Cheng <Javran.C@gmail.com>
2021-10-31 18:44:03 -0700

    use a better printer.

--- kantour/src/Kantour/ApiParser.hs
+++ kantour/src/Kantour/ApiParser.hs
@@ -3,6 +3,7 @@
 {-# OPTIONS_GHC -fdefer-typed-holes #-}
 
 module Kantour.ApiParser where
+
 import Data.Aeson as Aeson
 import qualified Data.ByteString.Lazy as BSL
 import Data.List
@@ -10,6 +11,7 @@ import Kantour.Core.KcData.Master.Root
 import Kantour.Subcommand
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
+import Shower
 import System.Environment
 import System.Exit
 
@@ -35,5 +37,5 @@ defaultMain =
       parsed <- case Aeson.eitherDecode @MasterRoot rawJson of
         Left msg -> die ("parse error: " <> msg)
         Right r -> pure r
-      print parsed
+      printer parsed
     _ -> die "<subcmd> [http]<source>"
