1f95fc8c467619017129be3b88af5ea45c375b53
Javran Cheng <Javran.C@gmail.com>
2021-11-03 20:23:01 -0700

    use disjoint set to classify ships into remodel sets.

--- kantour/src/Kantour/FetchMasterData.hs
+++ kantour/src/Kantour/FetchMasterData.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeApplications #-}
 {-# OPTIONS_GHC -fdefer-typed-holes #-}
 
@@ -10,10 +11,17 @@ module Kantour.FetchMasterData
   )
 where
 
+import Control.Monad
+import Control.Monad.ST
 import Data.Aeson as Aeson
 import qualified Data.ByteString.Lazy as BSL
 import qualified Data.IntMap as IM
+import qualified Data.IntSet as IS
 import Data.List
+import Data.Maybe
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import qualified Data.UnionFind.ST as UF
 import Kantour.Core.KcData.Master.Root
 import Kantour.Core.KcData.Master.Ship as Ship
 import Kantour.Subcommand
@@ -51,6 +59,16 @@ fetchMasterData mgr src =
       Left msg -> die ("parse error: " <> msg)
       Right r -> pure r
 
+-- cargoculting from myself: https://github.com/nominolo/union-find/issues/12#issuecomment-647090797
+cluster :: [(UF.Point s Int, b)] -> ST s (IM.IntMap [b])
+cluster ps =
+  IM.map ($ []) . IM.fromListWith (flip (.))
+    <$> mapM
+      (\(uf, c) -> do
+         cRep <- UF.descriptor =<< UF.repr uf
+         pure (cRep, (c :)))
+      ps
+
 {-
   TODO: do this later proper.
   TODO: we should probably have NFData on MasterRoot.
@@ -64,7 +82,13 @@ fetchMasterData mgr src =
  -}
 remodelChainExperiment :: MasterRoot -> IO ()
 remodelChainExperiment MasterRoot {mstShip} = do
-  let ships =
+  let afterShipIdToMaybe :: Maybe T.Text -> Maybe Int
+      afterShipIdToMaybe m = do
+        r0 <- m
+        [(v, "")] <- pure (reads $ T.unpack r0)
+        guard $ v /= 0
+        pure v
+      ships =
         IM.fromListWithKey
           (\k _ _ ->
              {-
@@ -74,6 +98,24 @@ remodelChainExperiment MasterRoot {mstShip} = do
           $ fmap (\s -> (Ship.shipId s, s)) allyShips
         where
           allyShips = filter ((<= 1500) . Ship.shipId) mstShip
+      remodelChains = runST $ do
+        -- TODO: use vector.
+        -- TODO: use IM.toList once rather than relying on IM.keys and IM.elems being consistent on ordering.
+        pointsPre <- mapM (\s -> UF.fresh s >>= \p -> pure (s, p)) (IM.keys ships)
+        let points = IM.fromList pointsPre
+        forM_ (IM.elems ships) $ \ship ->
+          case afterShipIdToMaybe . Ship.aftershipid $ ship of
+            Nothing -> pure ()
+            Just afterId -> do
+              UF.union (points IM.! Ship.shipId ship) (points IM.! afterId)
+        cluster $ zip (fmap snd pointsPre) (IM.elems ships)
+      noInDegShips = IS.difference (IM.keysSet ships) afterShipIds
+        where
+          afterShipIds =
+            IS.fromList
+              . mapMaybe (afterShipIdToMaybe . Ship.aftershipid)
+              . IM.elems
+              $ ships
   {-
     TODO: new algorithm:
 
@@ -88,7 +130,9 @@ remodelChainExperiment MasterRoot {mstShip} = do
       unattached ids should be alerted.
 
    -}
-  _todo
+  forM_ (IM.toList remodelChains) $ \(_k, vs) -> do
+    T.putStrLn $ T.unwords (fmap (\s -> Ship.name s <> "(" <> T.pack (show $ Ship.shipId s) <> ")") $ sortOn Ship.sortno vs)
+  print (afterShipIdToMaybe . Ship.aftershipid <$> IM.elems ships)
 
 defaultMain :: IO ()
 defaultMain =
