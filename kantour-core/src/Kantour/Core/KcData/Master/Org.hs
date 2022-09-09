6f4496673c89739c25a75204cb3f8e401e645369
Javran Cheng <Javran.C@gmail.com>
2022-09-08 21:50:45 -0700

    create a re-export module for Org.

---
+++ kantour-core/src/Kantour/Core/KcData/Master/Org.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+
+module Kantour.Core.KcData.Master.Org (
+  FromDirect (..),
+  module Kantour.Core.KcData.Master.Org.Root,
+) where
+
+-- TODO: export leaf modules.
+import Kantour.Core.KcData.Master.Org.Common
+import Kantour.Core.KcData.Master.Org.Root
+
