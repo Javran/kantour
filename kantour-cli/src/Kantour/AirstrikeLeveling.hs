9b8bf34c8749c3d53d80b1cff13997a589fc4be6
Javran Cheng <Javran.C@gmail.com>
2023-05-01 21:15:00 -0700

    this now looks correct.

--- kantour-cli/src/Kantour/AirstrikeLeveling.hs
+++ kantour-cli/src/Kantour/AirstrikeLeveling.hs
@@ -15,11 +15,9 @@ import qualified Kantour.Core.KcData.Master.Org.Ship as KcShip
 import Kantour.Subcommand
 import Network.HTTP.Client (newManager)
 import Network.HTTP.Client.TLS (tlsManagerSettings)
-import Shower
 import System.Environment
 import System.Exit
 import Text.Printf
-import Data.Coerce
 
 {-
   Tools for calculating resource consumption on 5-2C node.
@@ -29,6 +27,43 @@ import Data.Coerce
   ref: https://en.kancollewiki.net/Combat/Map_Mechanics_and_Nodes
 
   rsc consumption: fuel: 6%, ammo: 4%. rounded down but at least 1.
+
+  also:
+
+  e.prototype.getFuelForSupply = function () {
+    var bd = this.fuelMax - this.fuelNow
+    if (0 < (bd = Math.max(0, bd)) && this.isMarriage()) {
+      bd = Math.floor(0.85 * bd)
+      bd = Math.max(bd, 1)
+    }
+    return bd
+  }
+  e.prototype.getAmmoForSupply = function () {
+    var be = this.ammoMax - this.ammoNow
+    if (0 < (be = Math.max(0, be)) && this.isMarriage()) {
+      be = Math.floor(0.85 * be)
+      be = Math.max(be, 1)
+    }
+    return be
+  }
+
+obtain input:
+
+(() => {
+  const {info} = getStore()
+  const {ships, fleets} = info
+  const xs = fleets[0].api_ship
+
+  console.log(JSON.stringify(xs.map(x => {
+    const {api_lv: lv, api_ship_id: id} = ships[x]
+    return [id, lv]
+  })))
+})()
+
+example input looks like:
+
+> [[931,54],[285,135],[925,72],[185,43],[73,38],[372,45]]
+
  -}
 
 data SubCmdAirstrikeLeveling
@@ -39,63 +74,71 @@ instance Subcommand SubCmdAirstrikeLeveling where
 
 type FuelAmmo = (Int, Int)
 
-getShipInfo :: Org.Root -> Int -> Maybe (T.Text, FuelAmmo)
-getShipInfo Org.Root {Org.ships} x = do
+getShipInfo :: Org.Root -> (Int, Int) -> Maybe (T.Text, (Bool, FuelAmmo))
+getShipInfo Org.Root {Org.ships} (x, lvl) = do
   KcShip.Ship
     { KcShip.name = n
     , KcShip.ours = Just KcShip.SOurs {KcShip.fuelAmmoMax}
     } <-
     ships IM.!? x
-  pure (n, fuelAmmoMax)
+  pure (n, (lvl >= 100, fuelAmmoMax))
 
-airstrikeConsume :: FuelAmmo -> FuelAmmo -> Maybe (FuelAmmo, FuelAmmo) -- (after state, diff)
-airstrikeConsume (fuelMax, ammoMax) (fuel, ammo) = do
+airstrikeConsume :: (Bool, FuelAmmo) -> FuelAmmo -> Maybe (FuelAmmo, FuelAmmo) -- (after state, diff)
+airstrikeConsume (_mar, (fuelMax, ammoMax)) (fuel, ammo) = do
   guard $ fuel > 0 && ammo > 0
   let consume :: Double -> Int -> Int -> (Int, Int)
-      consume r mx x = (x - dx, dx)
+      consume r mx x = (x - cost, cost)
         where
-          dx = max 1 $ floor @Double @Int (r * fromIntegral mx)
+          cost = max 1 $ floor (fromIntegral mx * r)
       (fuel', df) = consume 0.06 fuelMax fuel
       (ammo', da) = consume 0.04 ammoMax ammo
   pure ((fuel', ammo'), (df, da))
 
-fleetConsume :: [FuelAmmo] -> [FuelAmmo] -> Maybe ([FuelAmmo], FuelAmmo)
+fleetConsume :: [(Bool, FuelAmmo)] -> [FuelAmmo] -> Maybe ([FuelAmmo], FuelAmmo)
 fleetConsume maxs xs = do
   ys <- zipWithM airstrikeConsume maxs xs
   let (df, da) = unzip $ fmap snd ys
   pure (fmap fst ys, (sum df, sum da))
 
-calculateAndDisplay :: [Int] -> IO ()
-calculateAndDisplay shipIds = do
+calculateAndDisplay :: [(Int, Int)] -> IO ()
+calculateAndDisplay ships = do
   mgr <- newManager tlsManagerSettings
   mstRootD <- fetchFromEnv (Just mgr)
   case runExcept $ runWriterT (Org.fromDirect @Org.Root mstRootD) of
     Left msg -> die $ "master data error: " <> T.unpack msg
     Right (root, _) -> do
-      let infos :: [(T.Text, FuelAmmo)]
-          infos = fmap (fromJust . getShipInfo root) shipIds
+      let infos :: [(T.Text, (Bool, FuelAmmo))]
+          infos = fmap (fromJust . getShipInfo root) ships
           mx = fmap snd infos
-      forM_ infos \(n, (x, y)) ->
-        printf "%s: fuel: %d, ammo: %d\n" n x y
+      forM_ infos \(n, (mar, (x, y))) ->
+        printf "%s: marriage: %c, fuel: %d, ammo: %d\n" n (if mar then 'y' else 'n') x y
 
       fix
-        ( \loop (cnt, st, tot :: (Sum Int, Sum Int)) -> do
+        ( \loop (cnt, st) -> do
             let cnt' = cnt + 1
             case fleetConsume mx st of
               Nothing -> pure ()
-              Just (st' :: [FuelAmmo], diff :: FuelAmmo) -> do
-                let tot' = tot <> coerce diff
-                -- TODO: wrong result
-                printf "%d: %s\n" cnt' (show $ coerce @_ @(Int, Int) tot')
-                loop (cnt', st', tot')
+              Just (st' :: [FuelAmmo], _) -> do
+                let (Sum fuelCost, Sum ammoCost) = mconcat $ zipWith computeCost infos st'
+                    computeCost :: (T.Text, (Bool, FuelAmmo)) -> FuelAmmo -> (Sum Int, Sum Int)
+                    computeCost (_, (mar, (fuMax, amMax))) (fu, am) = (Sum $ f fuMax fu, Sum $ f amMax am)
+                      where
+                        f cap x =
+                          if mar
+                            then max 1 $ floor @Double (0.85 * fromIntegral (cap - x))
+                            else cap - x
+
+                let ave v = fromIntegral v / (fromIntegral cnt' :: Double)
+                printf "%d: %s (%.2f, %.2f))\n" cnt' (show (fuelCost, ammoCost)) (ave fuelCost) (ave ammoCost)
+                loop (cnt', st')
         )
-        (0 :: Int, mx, (0, 0))
+        (0 :: Int, fmap snd mx)
 
 defaultMain :: IO ()
 defaultMain =
   getArgs
     >>= \case
-      [raw] -> case eitherDecode' @[Int] (BSLC.pack raw) of
+      [raw] -> case eitherDecode' @[(Int, Int)] (BSLC.pack raw) of
         Left msg -> die $ "decode error: " <> msg
         Right xs -> calculateAndDisplay xs
       _ -> die "<prog> '[ship ids, ...]'"
