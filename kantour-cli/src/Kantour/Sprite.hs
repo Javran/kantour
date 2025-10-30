731195afb17d96c4988b494d81cc6dc0e0dd1ef8
Javran Cheng <Javran.C@gmail.com>
2025-10-29 22:59:40 -0700

    few leftover notes that I forget to commit

--- kantour-cli/src/Kantour/Sprite.hs
+++ kantour-cli/src/Kantour/Sprite.hs
@@ -6,14 +6,29 @@ import Kantour.Subcommand
 import Shower
 import System.Environment
 import System.Exit (die)
+import Text.ParserCombinators.ReadP
 
 {-
-  TODO:
 
-  A tool for extracting spritesmith from kc assets:
+  A tool for extracting spritesmith from kc assets for:
 
   - general purpose sprite extraction
   - specialized for extracting ship tags for events
+
+  (TODO) Subcommands:
+
+  - `fetch <src> <dst dir>`: download from <src> to <dst dir>
+
+    + `w1:<relative url><optional query str>`: download from w01 server
+    + `http...`: download from a url
+
+    The url part (without query str) should ends with
+    either `.json` or `.png` - the tool should ensure both are fetched.
+
+  - `extract <json>`: load local JSON file, extract to the same directory
+    where that file resides.
+
+  - `tag-extract`: syntax TBD, extract ship tags in one command.
 
  -}
 
@@ -22,6 +37,8 @@ data SubCmdSprite
 instance Subcommand SubCmdSprite where
   name _ = "Sprite"
   main _ = defaultMain
+
+-- sourceP :: ReadP
 
 defaultMain :: IO ()
 defaultMain =
